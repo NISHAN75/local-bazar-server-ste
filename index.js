b471cf0536f4e8d484855be2015a024588ac9983
nishan das8 <nishandas880@gmail.com>
2022-09-16 23:32:44 +0600

    profile server is ok

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
@@ -6,30 +7,16 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 
-require("dotenv").config();
+
 
 const port = process.env.PORT || 5000;
-const server = http.createServer(app);
+
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-function verifyJwt(req, res, next) {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    return res.status(401).send({ message: "unauthorized access" });
-  }
-  const token = authHeader.split(" ")[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(403).send({ message: "Forbidden access" });
-    } else {
-      req.decoded = decoded;
-      next();
-    }
-  });
-}
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nsl6uid.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -47,6 +34,8 @@ async function run() {
     const ordersCollection = client.db("local_bazar").collection("orders");
     const paymentCollection = client.db("local_bazar").collection("payment");
     const usersCollection = client.db("local_bazar").collection("users");
+    const reviewsCollection = client.db("local_bazar").collection("reviews");
+    const profileCollection = client.db("local_bazar").collection("profile");
 
     // socal lo
 
@@ -115,6 +104,18 @@ async function run() {
       const result = await productsCollection.find(query).toArray();
       res.send(result);
     });
+    app.get("/reviews", async (req, res) => {
+      const query = {};
+      const cursor = reviewsCollection.find(query);
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    });
+     // profile
+     app.get('/profile/:email', async(req,res) =>{
+      const email = req.params.email;
+      const profile = await profileCollection.findOne({email: email});
+      res.send(profile);
+    });
     // all order
     
     app.get("/allOrders", async (req, res) => {
@@ -169,6 +170,17 @@ async function run() {
       const result = await ordersCollection.insertOne(orders);
       res.send(result);
     });
+     // review area
+     app.post("/reviews", async (req, res) => {
+      const reviews = req.body;
+      const result = await reviewsCollection.insertOne(reviews);
+      res.send(result);
+    });
+    app.post('/profile', async(req,res)=>{
+      const profile=req.body;
+      const result = await profileCollection.insertOne(profile);
+      res.send(result);
+    });
 
     // delete area
     app.delete('/orders', async(req,res)=>{
@@ -183,7 +195,12 @@ async function run() {
       const result = await ordersCollection.deleteOne(filter);
       res.send(result)
     });
-
+    app.delete('/products/:id', async(req,res)=>{
+      const id = req.params.id;
+      const filter={_id: ObjectId(id)};
+      const result = await productsCollection.deleteOne(filter);
+      res.send(result)
+    });
     
 
 
